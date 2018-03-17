e636354bf11f804ce354341bf98da0f75491c05d
user <user@corp-212-98-137-211.terra.net.lb>
2018-03-17 22:21:48 +0200

    json output

--- atlas_outage_plot.py
+++ atlas_outage_plot.py
@@ -2,7 +2,6 @@
 import json
 import requests
 import time
-import sys
 from ripe.atlas.cousteau import ProbeRequest
 from collections import defaultdict
 from bisect import bisect_right
@@ -53,23 +52,49 @@ def analyze_events(conn_event,disco_event,start_time):
             disco_duration[probe_id].append(int(conn_time)-nearest_disco_time)
     return(disco_duration)
 
+def plot_disco_duration(disco_duration,cc):
+    merge_disco_duration = []
+    for d in disco_duration.values():
+        merge_disco_duration = merge_disco_duration + d
+    fig, ax = plt.subplots()
+    ax.violinplot(np.log10(merge_disco_duration), showmedians=True)
+    #ax.set_yscale("log", nonposy='clip')
+    ax.grid(True)
+    ax.set_xticklabels([])
+    ax.set_xticklabels([])
+    plt.ylabel('log10(Disconnection duration in seconds)')
+    fig.savefig('disco_duration_dist_{}.png'.format(cc))
+    plt.close(fig)
+
+def plot_disco_time(disco_event):
+    merge_disco_time=[]
+    merge_disco_hour=[]
+    for d in disco_event.values():
+        merge_disco_time = merge_disco_time + d
+        for e in d: 
+            merge_disco_hour.append(datetime.fromtimestamp(e).hour)
+    fig, ax = plt.subplots()
+    merge_disco_hour = np.array(merge_disco_hour)
+    ax.hist(merge_disco_hour, bins=np.arange(merge_disco_hour.min(), merge_disco_hour.max()+1))
+    #ax.set_yscale("log", nonposy='clip')
+    ax.grid(True)
+    plt.ylabel('Disconnection time')
+    fig.savefig('disco_time_dist_{}.png'.format(cc))
+    plt.close(fig)
+
 if __name__ == "__main__":
-    cc = str(sys.argv[1])
-    #start_time = '1520630104'
-    start_time = '1518363868'
+    start_time = '1520630104'
+    #start_time = '1518363868'
     end_time = time.time()
+    cc = 'LB'
+    #cc = 'BE'
     conn_event, disco_event = get_events(cc,start_time,end_time)
-    conn_event_json = json.dumps(conn_event)
-    disco_event_json = json.dumps(disco_event)
     disco_duration = analyze_events(conn_event,disco_event,start_time)
+    #plot_disco_duration(disco_duration,cc)
+    #plot_disco_time(disco_event)
+    print(disco_duration)
     disco_duration_json = json.dumps(disco_duration)
-    f = open("conn_event_{}_{}_{}.json".format(cc,start_time,end_time),"w")
-    f.write(conn_event_json)
-    f.close()
-    f = open("disco_event_{}_{}_{}.json".format(cc,start_time,end_time),"w")
-    f.write(disco_event_json)
-    f.close()
-    f = open("disco_duration_{}_{}_{}.json".format(cc,start_time,end_time),"w")
+    f = open("disco_duration_{}.json".format(cc),"w")
     f.write(disco_duration_json)
     f.close()
     
